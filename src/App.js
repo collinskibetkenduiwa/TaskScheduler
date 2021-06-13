7826d10ba8e37c597b3f08b1fe6ada3d5380a27b
collinskibetkenduiwa <kibetcollinskenduiwa@gmail.com>
2021-06-13 12:53:40 +0300

    initial comit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="App">
+     
+      <h1>Hello from React</h1>
+
+      <h2>Its Collins learning React</h2>
+     
+    </div>
+  );
+}
+
+export default App;
